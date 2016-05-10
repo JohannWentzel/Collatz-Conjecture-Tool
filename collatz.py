1b8d0a24f0185fde0bb8cffac89815eb0f1b1ba9
JohannWentzel <wentzel142@gmail.com>
2016-05-10 13:52:52 -0700

    Added tutorial

--- collatz.py
+++ collatz.py
@@ -1,1 +1,18 @@
-num = input("Enter a number")
+done = False
+
+print("")
+print("- - - - - - - - - - -")
+print("COLLATZ CONJECTURE")
+print("- - - - - - - - - - -")
+print("")
+print("Enter a positive integer to get its Collatz path,")
+print("or press q to quit.")
+
+while done == False:
+    num = raw_input("->  ")
+    if type(num) is int:
+        
+    elif type(num) is str and num == "q":
+        done = True
+    else:
+        print("Please enter a positive integer.")
