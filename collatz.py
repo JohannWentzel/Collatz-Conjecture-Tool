9141915db5415684f034af4dad32194f67c9ec5b
JohannWentzel <wentzel142@gmail.com>
2016-05-10 15:07:07 -0700

    Rearranged input checking

--- collatz.py
+++ collatz.py
@@ -1,3 +1,11 @@
+# - - - - - - - - - - -
+#    COLLATZ.PY
+#  Johann Wentzel
+#   May 10, 2016
+# - - - - - - - - - - -
+
+import sys
+
 done = False
 errorMsg = "Please enter a positive integer, or q to quit."
 
@@ -10,28 +18,40 @@ print("")
 print("Enter a positive integer to get its Collatz path,")
 print("or enter q to quit.")
 
+def inputCheck( str ):
+    try:
+        num = int(rawNum)
+    # Catch non-string input.
+    except ValueError as e:
+        return False
+
+    # Check for positive int.
+    if num < 1:
+        return False
+
+    # Nothing has failed!
+    return True
+
 # Keep the tool looping until user quits.
 while done == False:
     rawNum = raw_input("->  ")
-    try:
-        n = int(rawNum)
-        if n <= 0:
-            print(errorMsg)
-        else:
-            print(n)
-            while (n != 1):
-                if n % 2 == 0:
-                    n = n / 2
-                else:
-                    n = (3 * n) + 1
-                print(n)
 
-    # Catch non-string input.
-    except ValueError as e:
-        # User quits
-        if rawNum == "q":
-            print("Quitting...")
-            done = True
-        # User enters invalid input
-        else:
-            print(errorMsg)
+    if rawNum == "q":
+        # Quit if the user chooses to.
+        print("Quitting...")
+        done = True
+
+    elif inputCheck(rawNum) == True:
+        # Run the actual logic!
+        n = int(rawNum)
+        print(n)
+        while (n != 1):
+            if n % 2 == 0:
+                n = n / 2
+            else:
+                n = (3 * n) + 1
+            print(n)
+
+    else:
+        # Something was entered wrong.
+        print(errorMsg)
