9f4197a8affb8be551c4417201431c136ef4203b
JohannWentzel <wentzel142@gmail.com>
2016-05-10 15:38:08 -0700

    Restructuring and refactoring

--- collatz.py
+++ collatz.py
@@ -6,19 +6,7 @@
 
 import sys
 
-done = False
-errorMsg = "Please enter a positive integer, or q to quit."
-
-# Tutorial prints.
-print("")
-print("- - - - - - - - - - -")
-print("COLLATZ CONJECTURE")
-print("- - - - - - - - - - -")
-print("")
-print("Enter a positive integer to get its Collatz path,")
-print("or enter q to quit.")
-
-def inputCheck( str ):
+def inputCheck( rawNum ):
     try:
         num = int(rawNum)
     # Catch non-string input.
@@ -32,26 +20,43 @@ def inputCheck( str ):
     # Nothing has failed!
     return True
 
-# Keep the tool looping until user quits.
-while done == False:
-    rawNum = raw_input("->  ")
 
-    if rawNum == "q":
-        # Quit if the user chooses to.
-        print("Quitting...")
-        done = True
 
-    elif inputCheck(rawNum) == True:
-        # Run the actual logic!
-        n = int(rawNum)
-        print(n)
-        while (n != 1):
-            if n % 2 == 0:
-                n = n / 2
-            else:
-                n = (3 * n) + 1
+def main(argv):
+
+    done = False
+    errorMsg = "Please enter a positive integer, or q to quit."
+
+    # Tutorial prints.
+    print("")
+    print("- - - - - - - - - - -")
+    print("COLLATZ CONJECTURE")
+    print("- - - - - - - - - - -")
+    print("Enter a positive integer to get its Collatz path,")
+    print("or enter q to quit.")
+
+    # Keep the tool looping until user quits.
+    while done == False:
+        rawNum = raw_input("->  ")
+
+        if rawNum == "q":
+            # Quit if the user chooses to.
+            print("Quitting...")
+            done = True
+
+        elif inputCheck(rawNum) == True:
+            # Run the actual logic!
+            n = int(rawNum)
             print(n)
+            while (n != 1):
+                if n % 2 == 0:
+                    n = n / 2
+                else:
+                    n = (3 * n) + 1
+                print(n)
 
-    else:
-        # Something was entered wrong.
-        print(errorMsg)
+        else:
+            # Something was entered wrong.
+            print(errorMsg)
+if __name__ == "__main__":
+    main(sys.argv)
